6387f856716609cc66ad257be11517f0c0cf3ad8
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-10-28 16:32:00 +0100

    Add JS validation for password match, change default submit behaviour

--- script/checkout.js
+++ script/checkout.js
@@ -1,4 +1,24 @@
 function validatePwd() {
-    let originalPassword = document.getElementById("pwd").ariaValueMax;
-    let repeatPassword = document.getElementById("pwd").ariaValueMax;
+    let originalPassword = document.getElementById("pwd")
+    let repeatPassword = document.getElementById("pwd-repeat")
+    if (originalPassword.value != repeatPassword.value) {
+        let label = document.getElementById("pwd-repeat-label");
+        label.innerHTML = "Passwords must match"
+        label.style.color = "red"
+        return false;
+    }
+    else {
+        return true;
+    }
+
 }
+
+let btn = document.getElementById("btn");
+btn.addEventListener("click",() => {
+    
+    if (validatePwd() == true) {
+        document.getElementById("submission-form").submit()
+    }
+
+
+});
